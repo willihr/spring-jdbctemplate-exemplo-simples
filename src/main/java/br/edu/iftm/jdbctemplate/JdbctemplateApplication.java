da997c33ab4083c6ac0a0cc3acc8705880d6372b
Willian Ribeiro <willihr10@gmail.com>
2021-09-15 21:42:23 -0300

    feat: print database basic info

--- src/main/java/br/edu/iftm/jdbctemplate/JdbctemplateApplication.java
+++ src/main/java/br/edu/iftm/jdbctemplate/JdbctemplateApplication.java
@@ -27,14 +27,12 @@ public class JdbctemplateApplication implements CommandLineRunner {
 		jdbcTemplate.update("INSERT INTO contatos(nome,telefone) VALUES (?,?)", "Edson Angoti Júnior", "123");
 		jdbcTemplate.update("INSERT INTO contatos(nome,telefone) VALUES (?,?)", "José Joaquim", "123");
 		jdbcTemplate.update("INSERT INTO contatos(nome,telefone) VALUES (?,?)", "Maria Carolina", "123");
-
-		List<Contato> contatos = jdbcTemplate.query("SELECT id, nome, telefone FROM contatos", (rs, rowNum) -> {
-			return new Contato(rs.getLong("id"), rs.getString("nome"), rs.getString("telefone"));
-		});
-		System.out.println("Listando contatos");
-		for (Contato contato : contatos) {
-			System.out.println(contato.getNome() + " - " + contato.getTelefone());
-		}
+		
+		int contagemDeLinhas = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM contatos", Integer.class);
+		System.out.printf("Há %d registros na tabela \"contatos\".%n", contagemDeLinhas);
+		
+		String telefone = jdbcTemplate.queryForObject("SELECT telefone FROM contatos WHERE nome = ?", String.class, "Edson Angoti Júnior");
+		System.out.printf("O telefone de Edson Angoti Júnior é %s.%n", telefone);
 	}
 
 }
